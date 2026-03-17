f1cdf8c4b7b03044b0bb78a6c36649c5f441e1bd
Pablo Grela <p.martinez.grela@accenture.com>
2026-03-17 21:50:54 +0100

    fix: cast the created script element to HTMLScriptElement for type safety

--- src/app/components/TestimonialsSection.tsx
+++ src/app/components/TestimonialsSection.tsx
@@ -93,7 +93,7 @@ export default function TestimonialsSection() {
     (function ($_x, _s, id) {
       var js, fjs = $_x.getElementsByTagName(_s)[0];
       if (!$_x.getElementById(id)) {
-        js = $_x.createElement(_s);
+        js = $_x.createElement(_s) as HTMLScriptElement;
         js.id = id;
         js.src = "//platform.docplanner.com/js/widget.js";
         // @ts-ignore
